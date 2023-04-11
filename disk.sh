235749ed56d3ad4f2584fe979ced33dcc4d72904
Kroese <kroese@users.noreply.github.com>
2023-04-12 01:52:55 +0200

    Mount ISO

--- disk.sh
+++ disk.sh
@@ -13,10 +13,10 @@ FILE="$IMG/data${DISK_SIZE}.img"
 [ ! -f "$FILE" ] && echo "ERROR: Data image does not exist ($FILE)" && exit 83
 
 KVM_DISK_OPTS="\
-    -device virtio-scsi-pci,id=hw-boot,bus=pcie.0,addr=0xa \
-    -drive file=${IMG}/boot.img,if=none,id=drive-boot,format=raw,cache=none,aio=native,discard=on,detect-zeroes=on \
-    -device scsi-hd,bus=hw-boot.0,channel=0,scsi-id=0,lun=0,drive=drive-boot,id=boot0,rotation_rate=1,bootindex=1 \
-    -device virtio-scsi-pci,id=hw-userdata,bus=pcie.0,addr=0xb \
+    -drive id=cdrom0,if=none,format=raw,readonly=on,file=${IMG}/boot.img \  
+    -device virtio-scsi-pci,id=scsi0 \  
+    -device scsi-cd,bus=scsi0.0,drive=cdrom0 \  
+    -device virtio-scsi-pci,id=hw-userdata,bus=pcie.0,addr=0xa \
     -drive file=${IMG}/data${DISK_SIZE}.img,if=none,id=drive-userdata,format=raw,cache=none,aio=native,discard=on,detect-zeroes=on \
-    -device scsi-hd,bus=hw-userdata.0,channel=0,scsi-id=0,lun=0,drive=drive-userdata,id=userdata0,rotation_rate=1,bootindex=2"
+    -device scsi-hd,bus=hw-userdata.0,channel=0,scsi-id=0,lun=0,drive=drive-userdata,id=userdata0,rotation_rate=1,bootindex=1"
 
