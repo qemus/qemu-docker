bf92f4a1f93f22ef18ac02563e6d3d3a9e879274
Kroese <kroese@users.noreply.github.com>
2023-04-15 18:09:20 +0200

    Error handling

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@ set -eu
 FILE="$IMG/boot.img"
 rm -f "$FILE"
 
-echo "Downloading $BOOT..."
+echo "Downloading ${BOOT}..."
 
 # Check if running with interactive TTY or redirected to docker log
 if [ -t 1 ]; then
-  wget "$BOOT" -O "$FILE".tmp -q --no-check-certificate --show-progress
+  wget "${BOOT}" -O "$FILE".tmp -q --no-check-certificate --show-progress
 else
-  wget "$BOOT" -O "$FILE".tmp -q --no-check-certificate --show-progress --progress=dot:giga
+  wget "${BOOT}" -O "$FILE".tmp -q --no-check-certificate --show-progress --progress=dot:giga
 fi
 
-[ ! -f "$FILE".tmp ] && echo "Download failed" && exit 61
+[ ! -f "$FILE".tmp ] && echo "Failed to download ${BOOT}" && exit 61
 
 mv -f "$FILE".tmp "$FILE"
