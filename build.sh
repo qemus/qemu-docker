9f408198a1ead6ae68c851f9ffba93a3c0a496f8
Kroese <kroese@users.noreply.github.com>
2023-04-16 06:42:54 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-docker build --tag qemu .
-docker images qemu:latest --format "{{.Repository}}:{{.Tag}} -> {{.Size}}"
-docker run --rm -it --name qemu --device="/dev/kvm" --cap-add NET_ADMIN -p 22:22 docker.io/library/qemu
-
