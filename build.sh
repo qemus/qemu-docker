f3ca9accc067cf949e2ef267294ee65f62685fe1
Kroese <kroese@users.noreply.github.com>
2023-04-16 02:05:16 +0200

    Trigger

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ set -e
 docker build --tag qemu .
 docker images qemu:latest --format "{{.Repository}}:{{.Tag}} -> {{.Size}}"
 docker run --rm -it --name qemu --device="/dev/kvm" --cap-add NET_ADMIN -p 22:22 docker.io/library/qemu
+
+trigger
