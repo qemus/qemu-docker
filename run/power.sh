d8669c0b8af2984c959f9c06de558daed4e2041f
Kroese <kroese@users.noreply.github.com>
2023-04-17 23:18:34 +0200

    Sync

--- run/power.sh
+++ run/power.sh
@@ -19,17 +19,7 @@ _trap(){
     done
 }
 
-snore()
-{
-    local IFS
-    [[ -n "${_snore_fd:-}" ]] || exec {_snore_fd}<> <(:)
-    read ${1:+-t "$1"} -u $_snore_fd || :
-}
-
 _graceful_shutdown(){
-
-  local QEMU_MONPORT="${QEMU_MONPORT:-7100}"
-  local QEMU_POWERDOWN_TIMEOUT="${QEMU_POWERDOWN_TIMEOUT:-8}"
 
   [ -f "${_QEMU_SHUTDOWN_COUNTER}" ] && return
 
@@ -40,7 +30,7 @@ _graceful_shutdown(){
   echo 0 > "${_QEMU_SHUTDOWN_COUNTER}"
 
   # Send the shutdown (system_powerdown) command to the QMP monitor
-  echo 'system_powerdown' | nc -q 1 -w 1 localhost "${QEMU_MONPORT}">/dev/null
+  echo 'system_powerdown' | nc -q 1 -w 1 localhost "${QEMU_MONPORT}" > /dev/null
 
   while [ "$(cat ${_QEMU_SHUTDOWN_COUNTER})" -lt "${QEMU_POWERDOWN_TIMEOUT}" ]; do
 
@@ -48,21 +38,22 @@ _graceful_shutdown(){
     echo $(($(cat ${_QEMU_SHUTDOWN_COUNTER})+1)) > ${_QEMU_SHUTDOWN_COUNTER}
 
     # Try to connect to qemu
-    if echo 'info version'| nc -q 1 -w 1 localhost "${QEMU_MONPORT:-7100}">/dev/null; then
+    if echo 'info version'| nc -q 1 -w 1 localhost "${QEMU_MONPORT}" > /dev/null; then
 
       sleep 1
       echo "Shutting down, waiting... ($(cat ${_QEMU_SHUTDOWN_COUNTER})/${QEMU_POWERDOWN_TIMEOUT})"
 
     fi
+
   done
 
   echo
   echo "Quitting..."
-  echo 'quit' | nc -q 1 -w 1 localhost "${QEMU_MONPORT:-7100}">/dev/null || true
+  echo 'quit' | nc -q 1 -w 1 localhost "${QEMU_MONPORT}" > /dev/null || true
 
   return
 }
 
 _trap _graceful_shutdown SIGTERM SIGHUP SIGINT SIGABRT SIGQUIT
 
-KVM_MON_OPTS="-monitor telnet:localhost:${QEMU_MONPORT:-7100},server,nowait,nodelay"
+KVM_MON_OPTS="-monitor telnet:localhost:${QEMU_MONPORT},server,nowait,nodelay"
