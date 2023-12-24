83aa25723e66d862ce06e730ce9b3f43d2294ba6
Kroese <kroese@users.noreply.github.com>
2023-12-24 14:41:42 +0100

    fix: Skip mknod errors

--- src/display.sh
+++ src/display.sh
@@ -27,23 +27,24 @@ if [[ "$GPU" != [Yy1]* ]] || [[ "$ARCH" != "amd64" ]]; then
   return 0
 fi
 
-DISPLAY_OPTS="$DISPLAY_OPTS -display egl-headless,rendernode=/dev/dri/renderD128"
-DISPLAY_OPTS="$DISPLAY_OPTS -device virtio-vga,id=video0,max_outputs=1,bus=pcie.0,addr=0x1"
-
 [ ! -d /dev/dri ] && mkdir -m 755 /dev/dri
 
 if [ ! -c /dev/dri/card0 ]; then
-  mknod /dev/dri/card0 c 226 0
+  if mknod /dev/dri/card0 c 226 0; then
+    chmod 666 /dev/dri/card0
+  fi
 fi
 
 if [ ! -c /dev/dri/renderD128 ]; then
-  mknod /dev/dri/renderD128 c 226 128
+  if mknod /dev/dri/renderD128 c 226 128; then
+    chmod 666 /dev/dri/renderD128
+  fi
 fi
-
-chmod 666 /dev/dri/card0
-chmod 666 /dev/dri/renderD128
 
 addPackage "xserver-xorg-video-intel" "Intel GPU drivers"
 addPackage "qemu-system-modules-opengl" "OpenGL module"
+
+DISPLAY_OPTS="$DISPLAY_OPTS -display egl-headless,rendernode=/dev/dri/renderD128"
+DISPLAY_OPTS="$DISPLAY_OPTS -device virtio-vga,id=video0,max_outputs=1,bus=pcie.0,addr=0x1"
 
 return 0
