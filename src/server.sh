9d588f452e49edae36049f0070e119e4880f7f7e
Kroese <kroese@users.noreply.github.com>
2024-01-19 23:35:25 +0100

    feat: HTML template

--- src/server.sh
+++ src/server.sh
@@ -13,16 +13,22 @@ trap 'stop' EXIT SIGINT SIGTERM SIGHUP
 
 html()
 {
+    local timeout="$3"
+    [ -z "$timeout" ] && timeout="4999"
+
+    local title="$1"
+    local body="$2<script>setTimeout(() => { document.location.reload(); }, $timeout);</script>"
+
     local HTML
     HTML=$(<"$template")
-    HTML="${HTML/[1]/$1}"
-    HTML="${HTML/[2]/$2}"
+    HTML="${HTML/[1]/$title}"
+    HTML="${HTML/[2]/$body}"
 
     printf '%b' "HTTP/1.1 200 OK\nContent-Length: ${#HTML}\nConnection: close\n\n$HTML" > "$page"
     return 0
 }
 
-html "$1" "$2<script>setTimeout(() => { document.location.reload(); }, $3);</script>"
+html "$1" "$2" "$3"
 
 socat TCP4-LISTEN:80,reuseaddr,fork,crlf SYSTEM:"cat $page" 2> /dev/null &
 socat TCP4-LISTEN:8006,reuseaddr,fork,crlf SYSTEM:"cat $page" 2> /dev/null & wait $!
