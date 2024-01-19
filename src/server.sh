6d14aefa5e0d82744b071219d92bba1b4660c418
Kroese <kroese@users.noreply.github.com>
2024-01-19 23:20:19 +0100

    feat: HTML template

--- src/server.sh
+++ src/server.sh
@@ -1,32 +1,30 @@
 #!/usr/bin/env bash
 set -eu
 
-TMP_FILE=$(mktemp -q /dev/shm/server.XXXXXX)
+page="/dev/shm/index.html"
+template="/var/www/index.html"
 
 stop() {
   trap - SIGINT EXIT
   { pkill -f socat || true; } 2>/dev/null
-  [ -f "$TMP_FILE" ] && rm -f "$TMP_FILE"
 }
 
 trap 'stop' EXIT SIGINT SIGTERM SIGHUP
 
 html()
 {
-    local h="<!DOCTYPE html><HTML><HEAD><TITLE>$2</TITLE>"
-    h="$h<STYLE>body { color: white; background-color: #125bdb; font-family: Verdana,"
-    h="$h Arial,sans-serif; } a, a:hover, a:active, a:visited { color: white; }</STYLE></HEAD>"
-    h="$h<BODY><BR><BR><H1><CENTER>$1</CENTER></H1></BODY></HTML>"
+    local HTML
+    HTML=$(<"$template")
+    HTML="${HTML/[1]/$1}"
+    HTML="${HTML/[2]/$2}"
 
-    echo "$h"
+    printf '%b' "HTTP/1.1 200 OK\nContent-Length: ${#HTML}\nConnection: close\n\n$HTML" > "$page"
+    return 0
 }
 
-BODY="$2<script>setTimeout(() => { document.location.reload(); }, 4999);</script>"
+html "$1" "$2<script>setTimeout(() => { document.location.reload(); }, $3);</script>"
 
-HTML=$(html "$BODY" "$1")
-printf '%b' "HTTP/1.1 200 OK\nContent-Length: ${#HTML}\nConnection: close\n\n$HTML" > "$TMP_FILE"
-
-socat TCP4-LISTEN:80,reuseaddr,fork,crlf SYSTEM:"cat $TMP_FILE" 2> /dev/null &
-socat TCP4-LISTEN:8006,reuseaddr,fork,crlf SYSTEM:"cat $TMP_FILE" 2> /dev/null & wait $!
+socat TCP4-LISTEN:80,reuseaddr,fork,crlf SYSTEM:"cat $page" 2> /dev/null &
+socat TCP4-LISTEN:8006,reuseaddr,fork,crlf SYSTEM:"cat $page" 2> /dev/null & wait $!
 
 exit
