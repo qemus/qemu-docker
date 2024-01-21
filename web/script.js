c1f4b5ca38d3a569fe142011fa909d1f630c6d91
Kroese <kroese@users.noreply.github.com>
2024-01-21 13:33:31 +0100

    Update script.js

--- web/script.js
+++ web/script.js
@@ -16,6 +16,7 @@ function getInfo() {
         request.open("GET", url, true);
         request.send();
     } catch (e) {
+        console.log(e.message);
         setInfo(e.message);
         reload();
     }
@@ -41,7 +42,7 @@ function processInfo() {
         }
 
         if (request.status == 404) {
-            setInfo("404");
+            setInfo("Connecting to web viewer", true);
             reload();
             return true;
         }
@@ -51,25 +52,34 @@ function processInfo() {
         return false;
 
     } catch (e) {
+        console.log(e.message);
         setInfo(e.message);
         reload();
         return false;
     }
 }
 
-function setInfo(text) {
+function setInfo(text, loading) {
+    
     try {
         if (text == null || text.length == 0) {
             return false;
         }
 
+        loading = !!loading;
+        if (loading) { 
+          text="<p class=\"loading\">" + text + "</p>"
+        }
+        
         var el = document.getElementById("info");
+        
         if (el.innerHTML != text) {
             el.innerHTML = text;
         }
 
         return true;
     } catch (e) {
+        console.log(e.message);
         return false;
     }
 }
