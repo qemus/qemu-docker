a45676e10510b4ccf11196f5b5ad644bf23a577b
Kroese <kroese@users.noreply.github.com>
2024-01-23 19:52:11 +0100

    feat: Make serial monitor configurable

--- src/config.sh
+++ src/config.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+: "${SERIAL:="mon:stdio"}"
+: "${MONITOR:="telnet:localhost:7100,server,nowait,nodelay"}"
+
 DEF_OPTS="-nodefaults"
-SERIAL_OPTS="-serial mon:stdio"
+SERIAL_OPTS="-serial $SERIAL"
+MON_OPTS="-monitor $MONITOR"
 USB_OPTS="-device qemu-xhci -device usb-tablet"
-MON_OPTS="-monitor telnet:localhost:7100,server,nowait,nodelay"
 RAM_OPTS=$(echo "-m $RAM_SIZE" | sed 's/MB/M/g;s/GB/G/g;s/TB/T/g')
 CPU_OPTS="-cpu $CPU_FLAGS -smp $CPU_CORES,sockets=1,dies=1,cores=$CPU_CORES,threads=1"
-MAC_OPTS="-machine type=q35${SECURE},graphics=off,vmport=off,dump-guest-core=off,hpet=off${KVM_OPTS}"
+MAC_OPTS="-machine type=q35${SECURE},vmport=off,dump-guest-core=off,hpet=off${KVM_OPTS}"
 DEV_OPTS="-device virtio-balloon-pci,id=balloon0,bus=pcie.0,addr=0x4"
 DEV_OPTS="$DEV_OPTS -object rng-random,id=objrng0,filename=/dev/urandom"
 DEV_OPTS="$DEV_OPTS -device virtio-rng-pci,rng=objrng0,id=rng0,bus=pcie.0,addr=0x1c"
