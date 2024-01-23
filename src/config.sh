2cfe9e84f12f5f10bd2fb28355acd1bb6061c0e7
Kroese <kroese@users.noreply.github.com>
2024-01-23 03:08:23 +0100

    Update config.sh

--- src/config.sh
+++ src/config.sh
@@ -16,8 +16,8 @@ ARGS="$DEF_OPTS $CPU_OPTS $RAM_OPTS $MAC_OPTS $DISPLAY_OPTS $MON_OPTS $SERIAL_OP
 ARGS=$(echo "$ARGS" | sed 's/\t/ /g' | tr -s ' ')
 
 if [[ "${DISPLAY,,}" == "web" ]]; then
-  rm -f /dev/shm/msg.html
-  rm -f /dev/shm/index.html
+  rm -f /run/shm/msg.html
+  rm -f /run/shm/index.html
 else
   if [[ "${DISPLAY,,}" == "vnc" ]]; then
     html "You can now connect to VNC on port 5900." "0"
