865cab7b248272117adf8a84e29650f5764b8f15
Kroese <kroese@users.noreply.github.com>
2024-02-02 11:15:56 +0100

    Update reset.sh

--- src/reset.sh
+++ src/reset.sh
@@ -39,7 +39,7 @@ HOST=$(hostname -s)
 KERNEL=$(uname -r | cut -b 1)
 MINOR=$(uname -r | cut -d '.' -f2)
 ARCH=$(dpkg --print-architecture)
-VERS=$(qemu-system-x86_64 --version | head -n 1 | cut -d '(' -f 1)
+VERS=$(qemu-system-arm --version | head -n 1 | cut -d '(' -f 1)
 
 # Check system
 
