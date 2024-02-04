bf7801ce65386d50bbb7a44f1486280cc9e8dfef
Kroese <kroese@users.noreply.github.com>
2024-02-04 14:52:31 +0100

    Update proc.sh

--- src/proc.sh
+++ src/proc.sh
@@ -43,15 +43,15 @@ if [[ "$KVM" != [Nn]* ]]; then
 
 else
 
+  KVM_OPTS=""
   CPU_FEATURES="l3-cache=on"
+  WIN_FEATURES="+hypervisor,hv_passthrough"
 
   if [[ "$ARCH" == "amd64" ]]; then
       CPU_MODEL="max"
       CPU_FEATURES="$CPU_FEATURES,migratable=no"
   fi
 
-  KVM_OPTS=""
-  WIN_FEATURES="+hypervisor,hv_passthrough"
   CPU_FEATURES="$CPU_FEATURES,+ssse3,+sse4.1,+sse4.2"
 
 fi
