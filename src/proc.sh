d928288234b7316136dbc34e3988392c3645bc11
Kroese <kroese@users.noreply.github.com>
2024-02-04 19:16:19 +0100

    feat: Multi-threaded TCG

--- src/proc.sh
+++ src/proc.sh
@@ -48,8 +48,9 @@ else
   WIN_FEATURES="+hypervisor,hv_passthrough"
 
   if [[ "$ARCH" == "amd64" ]]; then
-      CPU_MODEL="max"
-      CPU_FEATURES="$CPU_FEATURES,migratable=no"
+    CPU_MODEL="max"
+    KVM_OPTS=" -accel tcg,thread=multi"
+    CPU_FEATURES="$CPU_FEATURES,migratable=no"
   fi
 
   CPU_FEATURES="$CPU_FEATURES,+ssse3,+sse4.1,+sse4.2"
