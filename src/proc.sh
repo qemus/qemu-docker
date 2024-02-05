a75ca631a6964ea9622ddbb60cd40dcea0d4f057
Kroese <kroese@users.noreply.github.com>
2024-02-05 12:10:29 +0100

    Update proc.sh

--- src/proc.sh
+++ src/proc.sh
@@ -37,10 +37,9 @@ fi
 
 if [[ "$KVM" != [Nn]* ]]; then
 
-  KVM_OPTS=",accel=kvm -enable-kvm"
   CPU_FEATURES="kvm=on,l3-cache=on"
   WIN_FEATURES="+hypervisor,+invtsc,hv_passthrough"
-  BOOT_OPTS="$BOOT_OPTS -global kvm-pit.lost_tick_policy=discard"
+  KVM_OPTS=",accel=kvm -enable-kvm -global kvm-pit.lost_tick_policy=discard"
 
   if [ -z "$CPU_MODEL" ]; then
     CPU_MODEL="host"
