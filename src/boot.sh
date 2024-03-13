66a3f0e6f72387d9efac0e05e1545e5fddb28c74
Kroese <kroese@users.noreply.github.com>
2024-03-13 09:55:11 +0100

    feat: Add flag to disable secure boot (#450)

--- src/boot.sh
+++ src/boot.sh
@@ -21,6 +21,11 @@ case "${BOOT_MODE,,}" in
     ROM="OVMF_CODE_4M.ms.fd"
     VARS="OVMF_VARS_4M.ms.fd"
     ;;
+  windows_plain)
+    TPM="N"
+    ROM="OVMF_CODE_4M.fd"
+    VARS="OVMF_VARS_4M.fd"
+    ;;
   windows_legacy)
     USB="usb-ehci,id=ehci"
     ;;
@@ -48,7 +53,7 @@ if [[ "${BOOT_MODE,,}" != "legacy" ]] && [[ "${BOOT_MODE,,}" != "windows_legacy"
     cp "$OVMF/$VARS" "$DEST.vars"
   fi
 
-  if [[ "${BOOT_MODE,,}" != "uefi" ]]; then
+  if [[ "${BOOT_MODE,,}" == "secure" ]] || [[ "${BOOT_MODE,,}" == "windows" ]]; then
     SECURE=",smm=on"
     BOOT_OPTS="$BOOT_OPTS -global driver=cfi.pflash01,property=secure,value=on"
   fi
